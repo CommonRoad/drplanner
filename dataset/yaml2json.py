2ce1f605ef102310d0b538d3bfba50786c95ef17
Yuanfei <yuanfei.lin@tum.de>
2024-04-17 14:10:34 +0200

    add existed data pairs and readme

---
+++ dataset/yaml2json.py
@@ -0,0 +1,15 @@
+import yaml
+import json
+
+def yaml_to_json(yaml_file, json_file):
+    # Read the YAML file
+    with open(yaml_file, 'r') as file:
+        yaml_content = yaml.safe_load(file)
+
+    # Convert the YAML content to JSON and write to a file
+    with open(json_file, 'w') as file:
+        json.dump(yaml_content, file, indent=4)
+
+id = 9192001
+# Replace 'path/to/yaml.yaml' and 'path/to/json.json' with your file paths
+yaml_to_json(f'./raw/{str(id)}.yaml', f'./jsons/{str(id)}.json')

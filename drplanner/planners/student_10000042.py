1e1a3d5fa51499de3d3c11b3abb222dff91fcc65
Sebastian Illing <ge59zeg@mytum.de>
2024-05-16 18:49:44 +0200

    added local reactive-planner repo as source

--- drplanner/planners/student_10000042.py
+++ drplanner/planners/student_10000042.py
@@ -30,8 +30,6 @@ class DefaultCostFunction(CostFunction):
 
     def evaluate(self, trajectory: TrajectorySample):
         costs = 0.0
-        # acceleration costs
-        costs += np.sum((self.w_a * trajectory.cartesian.a) ** 2)
         # velocity costs
         if self.desired_speed is not None:
             costs += (
@@ -51,87 +49,4 @@ class DefaultCostFunction(CostFunction):
                 np.sum((0.25 * (self.desired_s - trajectory.curvilinear.s)) ** 2)
                 + (20 * (self.desired_s - trajectory.curvilinear.s[-1])) ** 2
             )
-
-        # distance costs
-        costs += (
-            np.sum((0.25 * (self.desired_d - trajectory.curvilinear.d)) ** 2)
-            + (20 * (self.desired_d - trajectory.curvilinear.d[-1])) ** 2
-        )
-        # orientation costs
-        costs += (
-            np.sum((0.25 * np.abs(trajectory.curvilinear.theta)) ** 2)
-            + (5 * (np.abs(trajectory.curvilinear.theta[-1]))) ** 2
-        )
-        cost
         return costs
-
-    @staticmethod
-    def acceleration_costs(trajectory: TrajectorySample) -> float:
-        """
-        Calculates the integral of acceleration squared.
-        """
-        acceleration = trajectory.cartesian.a
-        acceleration_sq = np.square(acceleration)
-        cost = simps(acceleration_sq, dx=trajectory.dt)
-
-        return cost
-
-    @staticmethod
-    def jerk_costs(trajectory: TrajectorySample) -> float:
-        """
-        Calculates the integral of jerk squared.
-        """
-        acceleration = trajectory.cartesian.a
-        jerk = np.diff(acceleration) / trajectory.dt
-        jerk_sq = np.square(jerk)
-        cost = simps(jerk_sq, dx=trajectory.dt)
-
-        return cost
-
-    @staticmethod
-    def lateral_jerk_costs(trajectory: TrajectorySample) -> float:
-        """
-        Same as jerk_costs but only for the lateral trajectory
-        """
-        cost = trajectory.trajectory_lat.squared_jerk_integral(trajectory.dt)
-        return cost
-
-    @staticmethod
-    def longitudinal_jerk_costs(trajectory: TrajectorySample) -> float:
-        """
-        Same as jerk_costs but only for the longitudinal_jerk_costs trajectory
-        """
-        cost = trajectory.trajectory_long.squared_jerk_integral(trajectory.dt)
-        return cost
-
-    @staticmethod
-    def orientation_offset_costs(trajectory: TrajectorySample) -> float:
-        """
-        Calculates the Orientation Offset cost.
-        """
-        theta = trajectory.curvilinear.theta
-        theta = np.diff(theta) / trajectory.dt
-        theta = np.square(theta)
-        cost = simps(theta, dx=trajectory.dt)
-
-        return cost
-
-    @staticmethod
-    def distance_to_reference_path_costs(trajectory: TrajectorySample) -> float:
-        """
-        Calculates the average lateral distance to the reference path,
-        but with a special emphasis on the final state
-        """
-        d = trajectory.curvilinear.d
-        cost = (np.sum(np.abs(d)) + np.abs(d[-1]) * 5) / len(d + 4)
-
-        return float(cost)
-
-    @staticmethod
-    def path_length_costs(trajectory: TrajectorySample) -> float:
-        """
-        Calculates the length of the given Trajectory
-        """
-        velocity = trajectory.cartesian.v
-        cost = simps(velocity, dx=trajectory.dt)
-        return cost

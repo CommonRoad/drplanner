d1e4cc062abf04a0aea4da57e77d37a16924fa52
Sebastian Illing <sebastian.illing@tum.de>
2024-06-01 11:04:15 +0200

    simplified the unique exceptions

--- drplanner/prompter/base.py
+++ drplanner/prompter/base.py
@@ -1,28 +1,28 @@
-# for dynamically construct the import statement
 import os
-import traceback
 from abc import ABC, abstractmethod
 
 from commonroad.scenario.scenario import Scenario
 from commonroad.planning.planning_problem import PlanningProblem
 
-from describer.base import ExceptionDescription, MissingParameterException
+from describer.base import (
+    ExceptionDescription,
+    DrPlannerException,
+)
 from drplanner.prompter.llm import LLM, LLMFunction
 from drplanner.describer.trajectory_description import TrajectoryCostDescription
-from describer.base import PlanningException, CompilerException
 
 from commonroad_dc.costs.evaluation import PlanningProblemCostResult
 
 
 class PrompterBase(ABC):
     def __init__(
-            self,
-            scenario: Scenario,
-            planning_problem: PlanningProblem,
-            api_key: str,
-            gpt_version: str = "gpt-3.5-turbo",  # gpt-3.5-turbo, text-davinci-002, gpt-4-1106-preview
-            prompts_folder_name: str = "astar/",
-            mockup=False,
+        self,
+        scenario: Scenario,
+        planning_problem: PlanningProblem,
+        api_key: str,
+        gpt_version: str = "gpt-3.5-turbo",  # gpt-3.5-turbo, text-davinci-002, gpt-4-1106-preview
+        prompts_folder_name: str = "astar/",
+        mockup=False,
     ):
         self.api_key = api_key
         self.gpt_version = gpt_version
@@ -43,22 +43,22 @@ class PrompterBase(ABC):
             self.prompt_system = file.read()
 
         with open(
-                os.path.join(script_dir, prompts_folder_name + "template.txt"), "r"
+            os.path.join(script_dir, prompts_folder_name + "template.txt"), "r"
         ) as file:
             self.algorithm_template = file.read()
 
         with open(
-                os.path.join(script_dir, prompts_folder_name + "constraints.txt"), "r"
+            os.path.join(script_dir, prompts_folder_name + "constraints.txt"), "r"
         ) as file:
             self.astar_constraints = file.read()
 
         with open(
-                os.path.join(script_dir, prompts_folder_name + "few_shots.txt"), "r"
+            os.path.join(script_dir, prompts_folder_name + "few_shots.txt"), "r"
         ) as file:
             self.astar_few_shots = file.read()
 
         with open(
-                os.path.join(script_dir, prompts_folder_name + "algorithm.txt"), "r"
+            os.path.join(script_dir, prompts_folder_name + "algorithm.txt"), "r"
         ) as file:
             self.astar_base = file.read()
 
@@ -84,7 +84,7 @@ class PrompterBase(ABC):
         pass
 
     def generate_cost_description(
-            self, cost_evaluation: PlanningProblemCostResult, desired_cost: float
+        self, cost_evaluation: PlanningProblemCostResult, desired_cost: float
     ):
         if not self.trajectory_description:
             self.trajectory_description = TrajectoryCostDescription(cost_evaluation)
@@ -101,18 +101,8 @@ class PrompterBase(ABC):
         description = "\n"
         description += "!AN EXCEPTION OCCURRED!\n"
 
-        if isinstance(e, PlanningException):
-            description += e.description + "\n"
-            description += ("You immediately need to change your prescriptions such that the planner does not crash "
-                            "anymore!")
-        elif isinstance(e, MissingParameterException):
-            description += e.description + "\n"
-            description += "To fix this you need to send the required parameter next time!"
-        elif isinstance(e, CompilerException):
-            description += "The compilation of the repaired python code failed:\n"
-            exp_des = ExceptionDescription(e.cause)
-            description += exp_des.generate()
-            description += "Next time please make sure that the python code you send is actually compilable"
+        if isinstance(e, DrPlannerException):
+            description += e.describe()
         else:
             exp_des = ExceptionDescription(e)
             description += str(e) + "\n"

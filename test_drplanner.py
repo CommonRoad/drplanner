47add9426365355b572b64c1cde98b8cb7173b53
Sebastian Illing <sebastian.illing@tum.de>
2024-06-02 19:09:51 +0200

    first testing base line version

--- test_drplanner.py
+++ test_drplanner.py
@@ -12,14 +12,16 @@ data = ["scenario", "gpt-version", "initial_cost", "final_cost"]
 for i in range(8):
     data.append(f"iter_{i}")
 filename = config.save_dir + "results.csv"
-file = open(filename, 'w+', newline='')
+file = open(filename, "w+", newline="")
 with file:
     write = csv.writer(file)
     write.writerow(data)
 
 # collect all scenarios
-scenarios_folder = "/home/sebastian/Documents/Uni/Bachelorarbeit/Scenarios/Datasets/Feasible"
-xml_files = glob.glob(os.path.join(scenarios_folder, '**', '*.xml'), recursive=True)
+scenarios_folder = (
+    "/home/sebastian/Documents/Uni/Bachelorarbeit/Scenarios/Datasets/Feasible"
+)
+xml_files = glob.glob(os.path.join(scenarios_folder, "**", "*.xml"), recursive=True)
 scenarios = [os.path.abspath(file) for file in xml_files]
 
 for p in scenarios:

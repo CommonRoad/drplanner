bfb3420a06e3a4fd5be5efcdc5671d4adaac9a77
Sebastian Illing <sebastian.illing@tum.de>
2024-06-03 15:53:10 +0200

    drSamplingPlanner v.0.2.0

--- test_drplanner.py
+++ test_drplanner.py
@@ -2,20 +2,62 @@ import csv
 import glob
 import os
 
-from drplanner.utils.config import DrPlannerConfiguration
-from run_drplanner import run_dr_sampling_planner
+from commonroad.common.file_reader import CommonRoadFileReader
+from drplanner.diagnostics.sampling import DrSamplingPlanner
 
-config = DrPlannerConfiguration()
+from drplanner.utils.config import DrPlannerConfiguration
+
+
+def results_to_csv(filename: str, data: list):
+    # opening the csv file in 'a+' mode
+    file = open(filename, "a+", newline="")
+    # writing the data into the file
+    with file:
+        write = csv.writer(file)
+        write.writerow(data)
+
+
+def run_dr_sampling_planner(scenario_filepath: str):
+    scenario, planning_problem_set = CommonRoadFileReader(scenario_filepath).open(True)
+    config = DrPlannerConfiguration()
+    dr_planner = DrSamplingPlanner(
+        scenario, scenario_filepath, planning_problem_set, config, "planner_id"
+    )
+    dr_planner.diagnose_repair()
+    filename = config.save_dir + "results.csv"
+
+    if len(dr_planner.cost_list) <= 0:
+        print("This scenario needs no repairs!")
+        row = [
+            str(scenario.scenario_id),
+            str(config.gpt_version),
+            dr_planner.initial_cost,
+        ]
+        results_to_csv(filename, row)
+
+    row = [
+        str(scenario.scenario_id),
+        str(config.gpt_version),
+        dr_planner.initial_cost,
+        min(dr_planner.cost_list),
+    ]
+    for c in dr_planner.cost_list:
+        row.append(c)
+    results_to_csv(filename, row)
+    print(dr_planner.cost_list, min(dr_planner.cost_list), dr_planner.initial_cost)
+
+
+drplanner_config = DrPlannerConfiguration()
 
 # initialize results.csv
-data = ["scenario", "gpt-version", "initial_cost", "final_cost"]
+row_data = ["scenario", "gpt-version", "initial_cost", "final_cost"]
 for i in range(8):
-    data.append(f"iter_{i}")
-filename = config.save_dir + "results.csv"
-file = open(filename, "w+", newline="")
-with file:
-    write = csv.writer(file)
-    write.writerow(data)
+    row_data.append(f"iter_{i}")
+result_filename = drplanner_config.save_dir + "results.csv"
+result_file = open(result_filename, "w+", newline="")
+with result_file:
+    w = csv.writer(result_file)
+    w.writerow(row_data)
 
 # collect all scenarios
 scenarios_folder = (

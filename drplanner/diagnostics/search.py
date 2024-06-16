7463e5e1a82aa390df907b7920214eab627e0ae6
ge62ton <sinuo.song@tum.com>
2024-06-16 16:43:32 +0200

    Version 3: run with memories.

--- drplanner/diagnostics/search.py
+++ drplanner/diagnostics/search.py
@@ -101,23 +101,16 @@ class DrSearchPlanner(DrPlannerBase):
             self.cost_type, VehicleType.BMW_320i
         )
 
-#通过迭代的方式，尝试生成一个满足条件的计划轨迹，如果在这个过程中发生了任何问题，这个方法会尝试修复这些问题
     def diagnose_repair(self):
-        #初始化迭代次数为0
+        
         nr_iteration = 0
-        #打印一条消息，表示诊断和修复过程开始
         print("[DrPlanner] Starts the diagnosis and repair process.")
         try:
-            #进行规划
             planned_trajectory = self.plan(nr_iteration)
-            #描述规划轨迹
             prompt_planner, evaluation_trajectory = self.describe(planned_trajectory)
-            #总成本赋值
             self.current_cost = evaluation_trajectory.total_costs
-        except:#except块中的代码处理try块中的代码可能抛出的异常
-            #调用describe方法描述一个空的规划轨迹
+        except:
             prompt_planner, _ = self.describe(None)
-            #将self.current_cost设置为无穷大
             self.current_cost = np.inf
         result = None
         self.initial_cost = self.current_cost
@@ -131,38 +124,29 @@ class DrSearchPlanner(DrPlannerBase):
 
         while (
             abs(self.current_cost - self.desired_cost) > self.THRESHOLD
-            #前成本与期望成本之间的绝对差值大于一个阈值。这意味着只有当当前成本与期望成本的差距足够大时，循环才会继续
             and self.token_count < self.TOKEN_LIMIT
-            #当前的令牌计数小于令牌限制。这可能是为了防止在一次迭代中使用过多的资源
             and nr_iteration < self.ITERATION_MAX
-            #当前的迭代次数小于最大迭代次数。这是为了防止无限循环
         ):
             print(f"*\t -----------iteration {nr_iteration}-----------")
             print("Retreive similar memories...")
-            #调用agent_memory.retriveMemory方法检索相似的记忆
 
             #todo:define the input metrics
             fewshot_results = self.updated_memory.retrieveMemory(  
-                scenario_description=scenario_description, top_k=self.few_shot_num) if self.few_shot_num > 0 else []  #sce是场景，如果few_shot_num大于0，就执行检索，否则fewshot_results为空列表
-            #初始化
+                scenario_description=scenario_description, top_k=self.few_shot_num) if self.few_shot_num > 0 else []  
             fewshot_messages = []
             fewshot_answers = []
-            #遍历fewshot_results
+            
             for fewshot_result in fewshot_results:
-                #对于每个结果，将其human_question、LLM_response和action字段分别添加到fewshot_messages、fewshot_answers和fewshot_actions列表中
                 fewshot_messages.append(fewshot_result["human_question"])
                 fewshot_answers.append(fewshot_result["LLM_response"])
             if self.few_shot_num == 0:
-                #表示现在处于零射击模式，没有少数派记忆
                 print("Now in the zero-shot mode, no few-shot memories.")
                 with_memory = False
             elif len(fewshot_messages) != 0:
-                #否则，打印一条消息，表示成功找到了fewshot_actions长度的相似记忆
                 print("Successfully find", len(
                     fewshot_messages), "similar memories!")
                 with_memory = True
             else:
-                #否则，打印一条消息，表示没有找到相似的记忆
                 print("No similar memories found!")
                 with_memory = False
                     
@@ -172,7 +156,6 @@ class DrSearchPlanner(DrPlannerBase):
                 f"*\t <{nr_iteration}>: total cost {self.current_cost} (desired: {self.desired_cost})\n"
                 f"*\t used tokens {self.token_count} (limit: {self.TOKEN_LIMIT})"
             )
-            #打印当前迭代次数、当前总成本、期望成本、已使用的token数和token限制
 
             message,human_message = self.human_question(
                 with_memory=with_memory,
@@ -183,16 +166,13 @@ class DrSearchPlanner(DrPlannerBase):
                 fewshot_answers=fewshot_answers,
             )
             
-            #创建一个消息列表，包含系统和用户的角色和内容
             
             # count the used token
             
-            #计算消息中的令牌数
             self.token_count += num_tokens_from_messages(
                 message, self.prompter.LLM.gpt_version
             )
             
-            #调用query方法执行查询，并将结果存储在result中
             result = self.prompter.LLM.query(
                 str(self.scenario.scenario_id),
                 str(self.planner_id),
@@ -202,17 +182,14 @@ class DrSearchPlanner(DrPlannerBase):
             )
 
             
-            #重新加载LLM
             self.prompter.reload_LLM()
             # add nr of iteration
             nr_iteration += 1
-            #将新的诊断和处方添加到prompt_planner
 
             diagnoses_prescriptions_records += (
                 f"*\t Diagnoses and prescriptions from the iteration {nr_iteration}:"
             )
             try:
-                #将result字典中的summary字段添加到prompt_planner字符串的末尾
                 diagnoses_prescriptions_records += f" {result['summary']}"
                 self.repair(result)
                 planned_trajectory = self.plan(nr_iteration)
@@ -232,15 +209,11 @@ class DrSearchPlanner(DrPlannerBase):
                 self.current_cost = np.inf
             finally:
                 if self.reflection:
-                    #如果REFLECTION为真，打印一条消息，表示正在运行反射代理
                     print("Now running reflection agent...")
-                    if abs(self.current_cost - self.desired_cost) > self.THRESHOLD: #如果当前成本大于期望成本[更新如果运行异常]
-                        #调用RA.reflection方法进行纠正，将纠正后的响应存储在corrected_response中
+                    if abs(self.current_cost - self.desired_cost) > self.THRESHOLD: 
                         corrected_response = RA.reflection(
                             human_message, result)
-                        #提示用户是否要添加这个新的记忆项来更新记忆模块
                         choice = input("Do you want to add this new memory item to update memory module? (Y/N): ").strip().upper()
-                        #如果用户输入的是'Y'，则调用updated_memory.addMemory方法添加记忆
                         if choice == 'Y':
                             self.updated_memory.addMemory(
                                 scenario_description,
@@ -249,27 +222,22 @@ class DrSearchPlanner(DrPlannerBase):
                                 comments="mistake-correction"
                             )
 
-                            #然后打印一条消息，表示成功添加了一个新的记忆项，现在数据库中有多少项
                             print("Successfully add a new memory item to update memory module. Now the database has ", len(
                                 self.updated_memory.scenario_memory._collection.get(include=['embeddings'])['embeddings']), " items.")
                         else:
-                            #如果用户输入的不是'Y'，则打印一条消息，表示忽略这个新的记忆项
                             print("Ignore this new memory item")
                         
                     else:
-                        #打印一条消息，询问用户是否要添加len(docs)//5个新的记忆项到记忆模块
                         print("Do you want to add 1 new memory item to update memory module?",end="")
                         choice = input("(Y/N): ").strip().upper()
                         if choice == 'Y':
-                             
-                            #就调用updated_memory.addMemory方法添加记忆
+                            
                             self.updated_memory.addMemory(
                                 scenario_description,
                                 human_message,
                                 result,
                                 comments="no-mistake-correction"
                             )
-                            #在循环结束后，打印一条消息，表示成功添加了cnt个新的记忆项，现在数据库中有多少个项
                             print("Successfully add 1 new memory item to update memory module.. Now the database has ", len(
                                         self.updated_memory.scenario_memory._collection.get(include=['embeddings'])['embeddings']), " items.")
                         else:
@@ -329,7 +297,7 @@ class DrSearchPlanner(DrPlannerBase):
         )
         return messages,human_message
     
-#根据诊断结果修复问题，包括更新启发式函数和运动原语，并创建一个新的StudentMotionPlanner实例
+
     def repair(self, diagnosis_result: Union[str, None]):
         # ----- heuristic function -----
         updated_heuristic_function = diagnosis_result[
@@ -389,24 +357,19 @@ class DrSearchPlanner(DrPlannerBase):
         )
         self.motion_planner.frontier = PriorityQueue()
 
-#生成计划轨迹的描述，包括规划器的描述和轨迹的描述。如果没有计划的轨迹，这个方法将生成一个表示没有轨迹的描述
+
     def describe(
             self, planned_trajectory: Union[Trajectory, None]
     ) -> (str, PlanningProblemCostResult):
-        #这是一个字符串模板，存储在template中
         template = self.prompter.astar_template
 
-        #生成规划器的描述
         planner_description = self.prompter.generate_planner_description(
             self.StudentMotionPlanner, self.motion_primitives_id
         )
-        #将template中的"[PLANNER]"替换为planner_description
         template = template.replace("[PLANNER]", planner_description)
 
         if planned_trajectory:
-            #评估轨迹
             evaluation_trajectory = self.evaluate_trajectory(planned_trajectory)
-            #生成轨迹的成本描述
             traj_description = self.prompter.generate_cost_description(
                 evaluation_trajectory, self.desired_cost
             )
@@ -416,7 +379,7 @@ class DrSearchPlanner(DrPlannerBase):
         template = template.replace("[PLANNED_TRAJECTORY]", traj_description)
         return template, evaluation_trajectory
 
-#根据更新后的轨迹和迭代次数，生成一段反馈信息，这段信息包括评估结果的描述，以及性能变化的信息。
+
     def add_feedback(self, updated_trajectory: Trajectory, iteration: int):
         feedback = "After applying this diagnostic result,"
         evaluation_trajectory = self.evaluate_trajectory(updated_trajectory)
@@ -438,15 +401,12 @@ class DrSearchPlanner(DrPlannerBase):
         self.current_cost = evaluation_trajectory.total_costs
         return feedback
 
-#执行搜索，生成轨迹，创建解决方案，可视化解决方案（如果需要），并保存解决方案（如果需要）
+
     def plan(self, nr_iter: int) -> Trajectory:
-        #执行搜索，并获取路径列表
         list_paths_primitives, _, _ = self.motion_planner.execute_search()
-        #根据路径列表创建轨迹
         trajectory_solution = create_trajectory_from_list_states(
             list_paths_primitives, self.motion_planner.rear_ax_dist
         )
-        #创建一个字典kwarg，其中包含了规划问题的ID、车辆模型、车辆类型、成本函数和轨迹
         kwarg = {
             "planning_problem_id": self.planning_problem.planning_problem_id,
             "vehicle_model": self.vehicle_model,
@@ -494,7 +454,7 @@ class DrSearchPlanner(DrPlannerBase):
             )
         return trajectory_solution
 
-#评估给定轨迹的成本
+
     def evaluate_trajectory(self, trajectory: Trajectory) -> PlanningProblemCostResult:
         return self.cost_evaluator.evaluate_pp_solution(
             self.scenario, self.planning_problem, trajectory

f7178d66e250e1cd9b7f9fe1ea70550a543e33ef
Yuanfei <yuanfei.lin@tum.de>
2024-10-15 13:53:56 +0200

    use new token counts from openai cookbook

--- dataset/yaml2json_all.py
+++ dataset/yaml2json_all.py
@@ -5,26 +5,28 @@ import os
 
 def yaml_to_json(yaml_file, json_file):
     # Read the YAML file
-    with open(yaml_file, 'r') as file:
+    with open(yaml_file, "r") as file:
         yaml_content = yaml.safe_load(file)
 
     # Convert the YAML content to JSON and write to a file
-    with open(json_file, 'w') as file:
+    with open(json_file, "w") as file:
         json.dump(yaml_content, file, indent=4)
 
 
-def convert_all_yaml_in_folder(folder_path, yaml_folder='raw', json_folder='jsons'):
+def convert_all_yaml_in_folder(folder_path, yaml_folder="raw", json_folder="jsons"):
     # Loop through each file in the directory
     yaml_folder = os.path.join(folder_path, yaml_folder)
     for filename in os.listdir(yaml_folder):
-        if filename.endswith('.yaml'):
+        if filename.endswith(".yaml"):
             # Construct the full file paths
             yaml_file = os.path.join(folder_path, yaml_folder, filename)
-            json_file = os.path.join(folder_path, json_folder, filename.replace('.yaml', '.json'))
+            json_file = os.path.join(
+                folder_path, json_folder, filename.replace(".yaml", ".json")
+            )
             print(yaml_file)
             # Convert the YAML file to JSON format
             yaml_to_json(yaml_file, json_file)
 
 
 # Replace 'path_to_folder' with the path of your folder containing the YAML files
-convert_all_yaml_in_folder('./')
+convert_all_yaml_in_folder("./")
